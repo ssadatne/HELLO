daf38e17cf6366382c6bba330598d233d69f358f
ssadatne <ssadatne@waltz.ee.ryerson.ca>
2015-10-23 17:02:29 -0400

    2add new line here

---
+++ LAB_5/src/LAB_5.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : LAB_5.cpp
+// Author      :
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl;
+	cout << "this is my niw line " ;// prints !!!Hello World!!!
+	return 0;
+}
